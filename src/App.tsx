82f9f78d0437d70fd9fb68e443b85fcc42b1c38e
Alexander Maat <amaat@byu.edu>
2024-03-09 12:47:35 -0700

    video6

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 
+interface BandProps {
+  name: string;
+  members: string;
+  formed: number;
+}
+
 const bandNames = [
   { name: 'The Beastie Boys', members: 'Mike D, MCA, Ad-Rock', formed: 1970 },
 
@@ -15,15 +21,39 @@ const bandNames = [
     members: 'Mick Jagger, Keith Richards, Charlie Watts, Ronnie Wood',
     formed: 1962,
   },
+
+  {
+    name: 'The Velvet Underground',
+    members: 'Lou Reed, John Cale, Sterling Morrison, Maureen Tucker',
+    formed: 1964,
+  },
+
+  {
+    name: 'The Who',
+    members: 'Roger Daltrey, Pete Townshend, John Entwistle, Keith Moon',
+    formed: 1964,
+  },
+
+  {
+    name: 'The Ramones',
+    members: 'Joey Ramone, Johnny Ramone, Dee Dee Ramone, Tommy Ramone',
+    formed: 1974,
+  },
+
+  {
+    name: 'The Clash',
+    members: 'Joe Strummer, Mick Jones, Paul Simonon, Topper Headon',
+    formed: 1976,
+  },
 ];
 
 function Welcome() {
   return <h1>Welcome to the Band List</h1>;
 }
 
-class Band extends React.Component {
+class Band extends React.Component<BandProps> {
   render() {
-    const oneBand = bandNames[0];
+    const oneBand = this.props;
 
     return (
       <div>
@@ -39,9 +69,9 @@ class Band extends React.Component {
 function BandList() {
   return (
     <div>
-      <Band />
-      <Band />
-      <Band />
+      {bandNames.map((bandNum) => (
+        <Band {...bandNum} />
+      ))}
     </div>
   );
 }
