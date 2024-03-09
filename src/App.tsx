c9ae38b6db80f34d341bfe6e604502792f69e9cc
Alexander Maat <amaat@byu.edu>
2024-03-09 12:01:51 -0700

    video 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const bandNames = [
+  { name: 'The Beastie Boys', members: 'Mike D, MCA, Ad-Rock', formed: 1970 },
+
+  {
+    name: 'The Beatles',
+    members: 'John Lennon, Paul McCartney, George Harrison, Ringo Starr',
+    formed: 1960,
+  },
+
+  {
+    name: 'The Rolling Stones',
+    members: 'Mick Jagger, Keith Richards, Charlie Watts, Ronnie Wood',
+    formed: 1962,
+  },
+];
+
+function Welcome() {
+  return <h1>Welcome to the Band List</h1>;
+}
+
+class Band extends React.Component {
+  render() {
+    const oneBand = bandNames[0];
+
+    return (
+      <div>
+        <img />
+        <h2>{oneBand.name}</h2>
+        <h2>{oneBand.members}</h2>
+        <h2>{oneBand.formed}</h2>
+      </div>
+    );
+  }
+}
+
+function BandList() {
+  return (
+    <div>
+      <Band />
+      <Band />
+      <Band />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <BandList />
     </div>
   );
 }
